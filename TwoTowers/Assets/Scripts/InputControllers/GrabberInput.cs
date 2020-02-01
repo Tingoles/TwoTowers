1a9f8012c367ade820c8cbeb8e03171d27f4527c
Unknown <connor.king3131@gmail.com>
2020-02-01 11:10:05 +0000

    Grabber Controller

---
+++ TwoTowers/Assets/Scripts/InputControllers/GrabberInput.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabberInput : MonoBehaviour
+{
+    [HideInInspector] public Vector3 m_inputVec;
+    [HideInInspector] public bool m_grabInput;
+}
