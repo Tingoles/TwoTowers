df5327c5f11324957ae03c4e2d9d709557cb8786
Benji Campbell <Benji.campbell@hotmail.co.uk>
2020-02-01 16:00:46 +0000

    Menu

---
+++ TwoTowers/Assets/Scripts/FX/UX/FadeColour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum Fade
+{
+    In,
+    Out
+}
+
+public class FadeColour : MonoBehaviour
+{
+    public Fade fade;
+    public float speed = 1;
+
+    private Image bg;
+
+    private void Start()
+    {
+        bg = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float target = 0;
+        switch(fade)
+        {
+            case Fade.In:
+                {
+                    target = 1;
+                    break;
+                }
+            case Fade.Out:
+                {
+                    target = 0;
+                    break;
+                }
+        }
+
+        bg.color = new Color(bg.color.r, bg.color.g, bg.color.b, Mathf.Lerp(bg.color.a, target, Time.deltaTime * speed));
+    }
+}
---
+++ TwoTowers/Assets/Scripts/GameData/Transition.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Transition : MonoBehaviour
+{
+    public bool sceneLoading = false;
+    public FadeColour background;
+
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(LoadSceneWithDelay(sceneName));
+    }
+
+    IEnumerator LoadSceneWithDelay(string sceneName)
+    {
+        sceneLoading = true;
+        background.fade = Fade.In;
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator SceneLoading()
+    {
+        //transition
+        yield return new WaitForSeconds(1.0f);
+        background.fade = Fade.Out;
+        sceneLoading = false;
+
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        StartCoroutine(SceneLoading());
+    }
+}
