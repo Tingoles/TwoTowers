bb1c766f778b2c26bdd1b756c166cf67a4ede7d3
Bryce <brycenewell1998@gmail.com>
2020-02-02 16:07:02 +0000

    Fence Sound

---
+++ TwoTowers/Assets/FenceFall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class FenceFall : MonoBehaviour
+{
+    bool playedOnce = false;
+    public float yPositionToggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(transform.position.y < yPositionToggle && !playedOnce)
+        {
+            playedOnce = true;
+            GetComponent<StudioEventEmitter>().Play();
+        }
+    }
+}
--- TwoTowers/Assets/Scripts/SpawnBuilding.cs
+++ TwoTowers/Assets/Scripts/SpawnBuilding.cs
@@ -77,6 +77,7 @@ public class SpawnBuilding : MonoBehaviour
         {
             Destroy(objToDelete);
         }
+
     }
 }
 
