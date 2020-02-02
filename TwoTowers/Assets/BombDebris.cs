faa69258e7abb708bbaba401bce1bc330103fa1c
Huw Tingley <Huw.Tingley@gmail.com>
2020-02-02 14:01:38 +0000

    bomb boxes added

---
+++ TwoTowers/Assets/BombDebris.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombDebris : MonoBehaviour
+{
+    public GameObject bomb;
+
+    BlockManager blockManager;
+
+    Vector3 bombPlacement;
+
+    private void Start()
+    {
+        blockManager = GameObject.FindGameObjectWithTag("BlockManager").GetComponent<BlockManager>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<BombDebris>())
+        {
+            blockManager.activeBlocks.Remove(collision.gameObject);
+            bombPlacement = collision.gameObject.transform.position;
+            Destroy(collision.gameObject);
+            GameObject temp = Instantiate(bomb);
+            temp.transform.position = bombPlacement;
+            Destroy(gameObject);
+        }
+    }
+}
